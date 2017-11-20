f7bce99ecda0b7b19e743d00f345cb39bb02c08c
Florian Delezenne <del.florian@gmail.com>
2017-11-20 15:51:15 +0100

    Add special redirections

--- routes/mails.js
+++ routes/mails.js
@@ -5,31 +5,50 @@ const express = require('express')
 const router = express.Router()
 const verification = require('../middlewares/slack')
 
-const help = `Commandes disponibles:\n\t\`/emails list\`\tliste des listes de diffusions existantes`
-
-function maillingListWithDescription(id) {
-  const description = descriptions[id] ? descriptions[id] : '[Ajoutez votre description](https://github.com/sgmap/slack-ovh/blob/master/descriptions.json)'
+function fillDescription(id) {
+  const description = descriptions[id] || 'Ajoutez votre description ici 👉 https://github.com/sgmap/slack-ovh/blob/master/descriptions.json'
   return `*${id}*: ${description}`
 }
 
 function toText(list) {
   return list.reduce((acc, item) => {
-    acc += `- ${maillingListWithDescription(item)}\n`
-    return acc
+    return acc + `- ${fillDescription(item)}\n`
   }, 'Listes de diffusions existantes:\n')
 }
 
-/* GET mailing-list listing. */
+function addSpecialRedirections(list) {
+  const specialRedirections = ['contact']
+
+  return list.concat(specialRedirections)
+}
+
+function list(res) {
+  return ovh.requestPromised('GET', `/email/domain/beta.gouv.fr/mailingList`)
+    .then(addSpecialRedirections)
+    .then(toText)
+    .then(text => res.send({
+      text,
+      mrkdwn: true,
+      response_type: 'ephemeral'
+    }))
+}
+
+function help(res) {
+  return res.send({
+    text: `Commandes disponibles:\n\t\`/emails list\`\tliste des listes de diffusions existantes`,
+    mrkdwn: true,
+    response_type: 'ephemeral'
+  })
+}
+
 router.post('/', verification, function(req, res, next) {
   const { text } = req.body
 
   switch(text) {
     case 'list':
-      return ovh.requestPromised('GET', `/email/domain/beta.gouv.fr/mailingList`)
-        .then(toText)
-        .then(text => res.send({ text, mrkdwn: true, response_type: 'ephemeral'}))
+      return list(res)
     default:
-      return res.send({ text: help, mrkdwn: true, response_type: 'ephemeral'})
+      return help(res)
   }
 })
 
