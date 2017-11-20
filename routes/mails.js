be7bb001f0f827e39f03a00969477088e06947c1
Florian Delezenne <del.florian@gmail.com>
2017-11-20 16:23:35 +0100

    Rejoindre, quitter une mailing-liste

--- routes/mails.js
+++ routes/mails.js
@@ -5,9 +5,31 @@ const express = require('express')
 const router = express.Router()
 const verification = require('../middlewares/slack')
 
+const specialRedirections = ['contact']
+
 function fillDescription(id) {
   const description = descriptions[id] || 'Ajoutez votre description ici 👉 https://github.com/sgmap/slack-ovh/blob/master/descriptions.json'
   return `*${id}*: ${description}`
+}
+
+function sendOk(res, mailingList, email) {
+  return () => {
+    return res.send({
+      text: `Inscription de *${email}* à la liste *${mailingList}* réussie.`,
+      mrkdwn: true,
+      response_type: 'ephemeral'
+    })
+  }
+}
+
+function sendError(res) {
+  return (err) => {
+    return res.send({
+      text: `Erreur: \`${JSON.stringify(err)}\`.`,
+      mrkdwn: true,
+      response_type: 'ephemeral'
+    })
+  }
 }
 
 function toText(list) {
@@ -17,8 +39,6 @@ function toText(list) {
 }
 
 function addSpecialRedirections(list) {
-  const specialRedirections = ['contact']
-
   return list.concat(specialRedirections)
 }
 
@@ -35,16 +55,74 @@ function list(res) {
 
 function help(res) {
   return res.send({
-    text: `Commandes disponibles:\n\t\`/emails list\`\tliste des listes de diffusions existantes`,
+    text: `Commandes disponibles:\n
+    \t\`/emails list\`\t\tliste des listes de diffusions existantes
+    \t\`/emails join <liste> <email>\`\trejoindre une liste
+    \t\`/emails leave <liste> <email>\`\tquitter une liste
+
+    Plus d'infos sur https://github.com/sgmap/slack-ovh`,
     mrkdwn: true,
     response_type: 'ephemeral'
   })
 }
 
-router.post('/', verification, function(req, res, next) {
-  const { text } = req.body
+function join(res, mailingList, email) {
+  if (specialRedirections.indexOf(mailingList) >= 0) {
+    // Add redirection
+    return ovh.requestPromised('POST', `/email/domain/beta.gouv.fr/redirection`, {
+      from: `${mailingList}@beta.gouv.fr`,
+      to: email,
+      localCopy: false
+    })
+    .then(sendOk(res, mailingList, email))
+    .catch(sendError(res))
+  } else {
+    // Subscribe from mailing-list
+    return ovh.requestPromised('POST', `/email/domain/beta.gouv.fr/mailingList/${mailingList}/subscriber`, { email })
+      .then(sendOk(res, mailingList, email))
+      .catch(sendError(res))
+  }
+}
 
-  switch(text) {
+function leave(res, mailingList, email) {
+  if (specialRedirections.indexOf(mailingList) >= 0) {
+    return res.send({
+      text: `Cette commande ne fonctionne pas encore, contactez #incubateur-ops pour obtenir de l'aide`,
+      mrkdwn: true,
+      response_type: 'ephemeral'
+    })
+
+    // Find existing redirection
+    // const redirection = {id: null} //TODO
+    // Remove redirection
+    // return ovh.requestPromised('DELETE', `/email/domain/beta.gouv.fr/redirection/${redirection.id}`)
+    //   .then(sendOk(res, mailingList, email))
+  } else {
+    // Unsubscribe from mailing-list
+    return ovh.requestPromised('DELETE', `/email/domain/beta.gouv.fr/mailingList/${mailingList}/subscriber/${email}`)
+      .then(_ => {
+        return res.send({
+          text: `Désinscription de *${email}* à la liste *${mailingList}* réussie.`,
+          mrkdwn: true,
+          response_type: 'ephemeral'
+        })
+      })
+      .catch(sendError(res))
+  }
+}
+
+router.post('/', verification, function(req, res, next) {
+  if (!req.body || !req.body.text) {
+    return help(res)
+  }
+
+  let [ cmd, mailingList, email ] = req.body.text.split(' ')
+
+  switch(cmd) {
+    case 'join':
+      return join(res, mailingList, email)
+    case 'leave':
+      return leave(res, mailingList, email)
     case 'list':
       return list(res)
     default:
