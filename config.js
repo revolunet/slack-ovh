867ba0fdba7ae4084e6112fb55c5a52b079a3bc4
Thomas Guillet <guillet.thomas@gmail.com>
2018-04-13 09:30:07 +0200

    Sort lists alphabetically

--- config.js
+++ config.js
@@ -9,13 +9,8 @@ const config = {
   slackChannel: 'incubateur-secretaria',
   lists: [
     {
-      id: "openacademie",
-      description: "Des solutions numériques pour l’administration scolaire, openacademie.beta.gouv.fr",
-      realMailingList: true
-    },
-    {
-      id: "incubateur",
-      description: "Tous les membres ayant un contrat en cours sur beta.gouv.fr/communaute",
+      id: "alpha",
+      description: "La formation à destination des acteurs de l'innovation du secteur public",
       realMailingList: true
     },
     {
@@ -24,8 +19,8 @@ const config = {
       realMailingList: true
     },
     {
-      id: "alpha",
-      description: "La formation à destination des acteurs de l'innovation du secteur public",
+      id: "compta",
+      description: "Achats, facturation, refacturation...",
       realMailingList: true
     },
     {
@@ -34,54 +29,59 @@ const config = {
       realMailingList: true
     },
     {
-      id: "compta",
-      description: "Achats, facturation, refacturation...",
-      realMailingList: true
-    },
-    {
-      id: "sgmap",
-      description: "Rares éléments administratifs nécessitant un lien contractuel (fiches de paie, gestion des ordres de mission…).",
-      realMailingList: true
+      id: "contact@aplus.beta.gouv.fr",
+      description: "Liste des redirections sur contact@aplus.beta.gouv.fr"
     },
     {
       id: "contact@beta.gouv.fr",
       description: "Liste des contacts sur beta.gouv.fr"
     },
     {
-      id: "recrutement@beta.gouv.fr",
-      description: "Tous les recrutements de beta.gouv.fr"
-    },
-    {
-      id: "contact@openacademie.beta.gouv.fr",
-      description: "Des solutions numériques pour l’administration scolaire, openacademie.beta.gouv.fr"
-    },
-    {
-      id: "contact@transport.beta.gouv.fr",
-      description: "Liste des redirections sur transport.beta.gouv.fr"
-    },
-    {
-      id: "contact@aplus.beta.gouv.fr",
-      description: "Liste des redirections sur contact@aplus.beta.gouv.fr"
-    },
-    {
-      id: "contact@locatio.beta.gouv.fr",
-      description: "Liste des redirections sur contact@locatio.beta.gouv.fr"
-    },
-    {
       id: "contact@code-du-travail.beta.gouv.fr",
       description: "L'équipe du code du travail numérique"
+    },
+    {
+      id: "contact@emjpm.beta.gouv.fr",
+      description: "Liste des redirections sur contact@emjpm.beta.gouv.fr"
     },
     {
       id: "contact@lebontuteur.beta.gouv.fr",
       description: "Liste des redirections sur contact@lebontuteur.beta.gouv.fr"
     },
     {
+      id: "contact@locatio.beta.gouv.fr",
+      description: "Liste des redirections sur contact@locatio.beta.gouv.fr"
+    },
+    {
+      id: "contact@openacademie.beta.gouv.fr",
+      description: "Des solutions numériques pour l’administration scolaire, openacademie.beta.gouv.fr"
+    },
+    {
       id: "contact@reso.beta.gouv.fr",
       description: "Liste des redirections sur contact@reso.beta.gouv.fr"
     },
     {
-      id: "contact@emjpm.beta.gouv.fr",
-      description: "Liste des redirections sur contact@emjpm.beta.gouv.fr"
+      id: "contact@transport.beta.gouv.fr",
+      description: "Liste des redirections sur transport.beta.gouv.fr"
+    },
+    {
+      id: "incubateur",
+      description: "Tous les membres ayant un contrat en cours sur beta.gouv.fr/communaute",
+      realMailingList: true
+    },
+    {
+      id: "openacademie",
+      description: "Des solutions numériques pour l’administration scolaire, openacademie.beta.gouv.fr",
+      realMailingList: true
+    },
+    {
+      id: "recrutement@beta.gouv.fr",
+      description: "Tous les recrutements de beta.gouv.fr"
+    },
+    {
+      id: "sgmap",
+      description: "Rares éléments administratifs nécessitant un lien contractuel (fiches de paie, gestion des ordres de mission…).",
+      realMailingList: true
     }
   ]
 }
